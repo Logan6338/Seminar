7820cca7ef5869c194597de6fbd0122a5d8c1797
Dmitry <Logan6338@mail.ru>
2022-08-16 19:48:14 +0300

    Замена в тексте

---
+++ ZamenaVText/Program.cs
@@ -0,0 +1,25 @@
+﻿string text ="- Я думаю, - сказал князь, улыбаясь, - что, "
+            +"ежеле бы вас поставили вместо нашего милого Винцергероду,";
+
+//string s = "qwert"
+//            012 
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+    
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(text, 'к', 'К');
+Console.WriteLine(newText);
